7e55cabd7b05b2cf16e186b8bd2529045a516f6b
Лозов Пётр <not_kill@mail.ru>
2013-09-24 18:29:28 +0400

    Испрвил комментарии

--- Программы на Haskell/ControlWork.hs
+++ Программы на Haskell/ControlWork.hs
@@ -1,35 +1,35 @@
-  -- Задание №01 --
+п»ї  -- Р—Р°РґР°РЅРёРµ в„–01 --
 
 zipWith' f (x:xs) (y:ys) = (f x y) : (zipWith' f xs ys)
 zipWith' f l1 l2 = []
 
-  -- Задание №02 --
+  -- Р—Р°РґР°РЅРёРµ в„–02 --
 
 sprod x y  = foldl (+) 0 (zipWith' (*) x y) 
 
-  -- Задание №03 --
+  -- Р—Р°РґР°РЅРёРµ в„–03 --
 
 normalize (x:xs) = x : (normalize $ filter (/=x) xs)
 normalize [] = []
 
-  -- Задание №04 --
+  -- Р—Р°РґР°РЅРёРµ в„–04 --
 
 sort (x:xs) = (sort $ filter (<=x) xs) ++ [x] ++ (sort $ filter (>x) xs)
 sort [] = []
 
-  -- Задание №05 --
+  -- Р—Р°РґР°РЅРёРµ в„–05 --
 
 isProgression (x1:xs@(x2:_)) = isProg xs where
   isProg (y1:ys@(y2:_)) = if y2 - y1 == diff then isProg ys else False where diff = x2 - x1
   isProg y = True   
 isProgression x = True
 
-  -- Задание №06 --  
+  -- Р—Р°РґР°РЅРёРµ в„–06 --  
 
 isFunction ((x,_):xs) = (foldl (\acc (s, _) -> acc && (s /= x)) True xs) && isFunction xs
 isFunction [] = True
 
-  -- Задание №07 --  
+  -- Р—Р°РґР°РЅРёРµ в„–07 --  
 
 isSymmetric ((x,y):xs) 
   | x == y = isSymmetric xs 
@@ -38,7 +38,7 @@ isSymmetric ((x,y):xs)
     where res = foldr (\s (acc,isSym) -> if s == (y,x) then (acc, True) else (s:acc,isSym)) ([], False) xs
 isSymmetric [] = True
 
-  -- Задание №08 -- 
+  -- Р—Р°РґР°РЅРёРµ в„–08 -- 
 
 isReflexive r = foldr fun True [1..] where 
   fun a acc = (elem (s,s) r) && acc where
@@ -48,10 +48,10 @@ isReflexive' r = isRef (normalize $ foldl (\acc (a,b) -> a:b:acc) [] r) where
   isRef (h:hs) = (foldl (\acc (a, b) -> if (h == a) && (a == b) then True else acc) False r) && isRef hs
   isRef [] = True
   
-  -- Задание №09 -- 
+  -- Р—Р°РґР°РЅРёРµ в„–09 -- 
 
 closure r = r ++ [(a,d) | (a,b) <- r, (c,d) <- r, b == c, not $ elem (a,d) r]
 
-  -- Задание №10 -- 
+  -- Р—Р°РґР°РЅРёРµ в„–10 -- 
   
 isTransitive x = x == (closure x)
--- Программы на Haskell/test.hs
+++
@@ -1,4 +0,0 @@
-﻿sqr q = sqr' q where 
-  sqr' y = if y * y / q < e then y 
-                             else sqr' ((y + q / y) / 2) where 
-                               e = 1.0001
