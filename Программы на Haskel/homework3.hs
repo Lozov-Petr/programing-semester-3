356145948de5497b647c841b46db673934cd2f06
Лозов Пётр <not_kill@mail.ru>
2013-09-24 15:51:52 +0900

    Исправил небольшую ошибку

--- Программы на Haskel/homework3.hs
+++ Программы на Haskel/homework3.hs
@@ -1,4 +1,4 @@
 fib = map (fst) l where l = ((0, 1) : [(b, a + b) | (a, b) <- l])
 
-primes1 = 2 : [x | x <- [3,5..], is_prime x] where 
+primes = 2 : [x | x <- [3,5..], is_prime x] where 
   is_prime x = foldr (\l acc -> l * l > x || (acc && rem x l /= 0)) True primes
