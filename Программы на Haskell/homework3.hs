c2756962481eeffa474b6b4e2a8629595ac539f2
Лозов Пётр <not_kill@mail.ru>
2013-09-25 14:06:18 +0400

    Новый бесконечный список простых чисел

--- Программы на Haskell/homework3.hs
+++ Программы на Haskell/homework3.hs
@@ -3,4 +3,4 @@ fib = map fst l where l = (0, 1) : [(b, a + b) | (a, b) <- l]
 primes = 2 : [x | x <- [3,5..], is_prime x] where 
   is_prime x = foldr (\l acc -> l * l > x || acc && rem x l /= 0) True primes
   
-  
+primes' = s [2..] where s (x:xs) = x : s (filter (\a -> rem a x /= 0) xs)
