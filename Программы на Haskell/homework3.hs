5c4a8c467e8c778f5adb803d23d1be0846e5b9a9
Лозов Пётр <not_kill@mail.ru>
2013-09-25 13:18:24 +0400

    Убрал лишние скобки

--- Программы на Haskell/homework3.hs
+++ Программы на Haskell/homework3.hs
@@ -1,4 +1,6 @@
-fib = map (fst) l where l = ((0, 1) : [(b, a + b) | (a, b) <- l])
+fib = map fst l where l = (0, 1) : [(b, a + b) | (a, b) <- l]
 
 primes = 2 : [x | x <- [3,5..], is_prime x] where 
-  is_prime x = foldr (\l acc -> l * l > x || (acc && rem x l /= 0)) True primes
+  is_prime x = foldr (\l acc -> l * l > x || acc && rem x l /= 0) True primes
+  
+  
