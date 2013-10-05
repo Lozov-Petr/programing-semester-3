301d35c6a1aacb7d2e4a5cc34d519668ed8f218d
Лозов Пётр <not_kill@mail.ru>
2013-10-05 13:04:45 +0400

    Новая isTree

--- Программы на Haskell/homework4.hs
+++ Программы на Haskell/homework4.hs
@@ -9,15 +9,20 @@ find tree@(T l u r) x
   | x == u = Just tree
 find E x = Nothing
 
-elements x = el x [] where
+elements tree = el tree [] where
   el (T l u r) acc = el l (u:(el r acc))
   el E acc = acc    
 
-elements1 (T l u r) = (elements l) ++ [u] ++ (elements r)
+isTree tree = isT tree Nothing Nothing where
+  isT (T l u r) min max = min <= u' && (max == Nothing || max > u') && isT l min u' && isT r u' max where u' = Just u
+  isT E _ _ = True
+
+
+elements1 (T l u r) = (elements1 l) ++ [u] ++ (elements1 r)
 elements1 E = []
 
-isTree E = True
-isTree tree = a where 
+isTree2 E = True
+isTree2 tree = a where 
   (a,_,_) = isTree' tree where
     isTree' (T E u E) = (True,u,u)
     isTree' (T l u E) = (maxL < u && b,minL,u) where (b,minL,maxL) = isTree' l
@@ -25,6 +30,7 @@ isTree tree = a where
     isTree' (T l u r) = (u <= minR && maxL < u && b1 && b2,minL,maxR) where 
       (b1,minL,maxL) = isTree' l
       (b2,minR,maxR) = isTree' r
+    
 
 isTree1 tree = foldr (\x acc -> x >= 0 && acc) True $ zipWith (-) xs x where x@(_:xs) = elements tree 
 --isTree1 - плохая функция, так как T((T E 1 E) 1 (T E 1 E)) -> True
@@ -40,5 +46,9 @@ treeToString (T l u r) = "T(" ++ (treeToString l) ++ " " ++ (show u) ++ " " ++ (
 maybeTreeToString Nothing = "Nothing"
 maybeTreeToString (Just x) = "Just " ++ (treeToString x) 
 
-tree = createTree [8,4,12,2,6,10,14,1,3,5,7,9,11,13,15]
-tree1 = T (T (T E 4 E) 4 E) 5 (T E 6 (T E 6 E))
+
+tree = T (T (T (T (T (T E 5 E) 6 E) 7 E) 8 E) 9 E) 10 E
+tree0 = createTree (2500:1250:3750:875:1625:3375:4125:[0,100..5000])
+tree1 = createTree [8,4,12,2,6,10,14,1,3,5,7,9,11,13,15]
+tree2 = T (T (T (T E 3 E) 3 E) 4 E) 5 (T E 6 (T E 6 E))
+tree3 = createTree [2,3,2]
