6405efa81e7bc7114b39ca51329fe81d32baa90a
Лозов Пётр <not_kill@mail.ru>
2013-10-07 18:47:24 +0400

    Добавлено Circle.

---
+++ Программы на C#/Circle/CircleLib/CircleLib.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircleLib
+{
+    public class Circle
+    {
+        public int X;
+        public int Y;
+        public int R;
+
+        public Circle(int X, int Y, int R)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.R = R;
+        }
+
+        public bool Intersect(Circle c)
+        {
+            return Math.Sqrt(Math.Pow(this.X - c.X, 2) + Math.Pow(this.Y - c.Y, 2))
+                <= this.R + c.R;
+        }
+    }
+}
---
+++ Программы на C#/Circle/IntersectCircles/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CircleLib;
+
+namespace Shell
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Circle c1 = CreateCircle();
+            Circle c2 = CreateCircle();
+            pictureBox.CreateGraphics().Clear(pictureBox.BackColor);
+            DrowCircle(c1);
+            DrowCircle(c2);
+            if (c1.Intersect(c2)) label.Text = "Круги пересекаются";
+            else label.Text = "Круги не пересекаются";
+        }
+
+        private Circle CreateCircle()
+        {
+            int R = rnd.Next(10, 65);
+            return new Circle(rnd.Next(R, 200 - R), rnd.Next(R, 200 - R), R);
+        }
+
+        public void DrowCircle(Circle c)
+        {
+            pictureBox.CreateGraphics().DrawEllipse(new Pen(Color.Chocolate, 2), 
+                c.X - c.R,c.Y - c.R, 2 * c.R, 2 * c.R);
+        }
+    }
+}
