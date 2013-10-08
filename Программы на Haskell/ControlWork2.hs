6ab924be03f355b74b415b96a122ab44eb16ffc5
Лозов Пётр <not_kill@mail.ru>
2013-10-08 18:12:28 +0400

    Дооформил

--- Программы на Haskell/ControlWork2.hs
+++ Программы на Haskell/ControlWork2.hs
@@ -6,15 +6,15 @@ dom list = map fst list
 
 eval :: [(Integer, Integer)] -> Integer -> Integer
 eval ((a,b):ls) x = if a == x then b else eval ls x
-eval [] x = error ("function is not defined for values " ++ show x)
+eval [] x = error ("Function is not defined for values " ++ show x ++ ".")
 
 invert :: [(Integer, Integer)] -> [(Integer, Integer)]
-invert list = map (\(a,b) -> (b,a)) list
+invert fun = map (\(a,b) -> (b,a)) fun
 
 (.*.) :: [(Integer, Integer)] -> [(Integer, Integer)] -> [(Integer, Integer)] 
 infixr 9 .*.
-l1 .*. ((a,b):ls) = if elem == Nothing then l1 .*. ls else ((\(Just (c,d)) -> (a,d)) elem):(l1 .*. ls) where 
-  elem = foldr (\r@(c,d) acc -> if c == b then Just r else acc) Nothing l1 where
+l1 .*. ((a,b):ls) = if elem == Nothing then l1 .*. ls else ((\(Just d) -> (a,d)) elem):(l1 .*. ls) where 
+  elem = foldr (\(c,d) acc -> if c == b then Just d else acc) Nothing l1 where
 l1 .*. [] = []
 
 image :: [(Integer, Integer)] -> [Integer] -> [Integer]
@@ -35,6 +35,6 @@ areMatuallyInverse l1 l2 = len1 == length l2 && len1 == length comp && filter (\
   comp = l1 .*. l2
   len1 = length l1  
   
-testFun1 = map (\x -> (x, x + 1)) [0..]
-testFun2 = map (\x -> (x, x * 2)) [0..]
-testFun3 = map (\x -> (x, 0)) [0..]
+testFun1 = [(x,x + 1) | x <- [0..]]
+testFun2 = [(x,x * 2) | x <- [0..]]
+testFun3 = [(x,    0) | x <- [0..]]
