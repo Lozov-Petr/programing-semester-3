e2be7aeb7230e56fc8d563a64737dbaa38f1c04b
Лозов Пётр <not_kill@mail.ru>
2013-10-23 02:21:07 +0400

    Пара 22.10.13

---
+++ Haskell/para4.hs
@@ -0,0 +1,15 @@
+data Map a b = M [(a, b)] 
+
+empty = M []
+
+insert (M list) a b = M ((a,b):list)
+
+createMap l1 l2 = M (zip l1 l2)
+
+find (M list) a = foldl (\acc (s,b) -> if a == s then Just b else acc) Nothing list
+
+remove (M list) a = M (rem list) where
+  rem [] = []
+  rem (x@(s,b):xs) = if s == a then xs else x:(rem xs)
+ 
+fold f (M list) acc = foldl f' acc list where f' c (a,b) = f a b c
