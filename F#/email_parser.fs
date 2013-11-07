526c80846d384df22ff39a374cb5ba32320e514d
Лозов Пётр <not_kill@mail.ru>
2013-11-08 02:37:46 +0400

    Исправил парсер и добавил модульное тестирование.

--- F#/email_parser.fs
+++ F#/email_parser.fs
@@ -1,23 +1,85 @@
-open System.Text.RegularExpressions
+﻿(******************************
+           Лозов Пётр
+           Группа 271
+            08.11.13
+        Парсер e-mail'ов
+   [+ модульное тестирование]
+ *******************************)
 
-let is_properly_email str =
-    let str_domens = "((aero)|(asia)|(coop)|(info)|(jobs)|(mobi)|(museum)|(name)|(travel)|([a-z]{2,3}))"
-    let sample = "([a-zA-Z_0-9]+\.?)+[a-zA-Z0-9_-]+@([a-z]+\.)+" + str_domens
+open System.Text.RegularExpressions
+open NUnit.Framework
+open FsUnit
+
+let isProperlyEmail str =
+    let strDomens = "(aero|asia|coop|info|jobs|mobi|museum|name|travel|[a-z]{2,3})"
+    let sample = "[a-zA-Z_][a-zA-Z0-9_-]*(\.?[a-zA-Z_0-9]+)*@([a-z]+\.)+" + strDomens
     ((new Regex("^" + sample  + "$")).Match str).Success
 
-// right examples
-printfn "%A" (is_properly_email "LoZoV.PeTeR@spb.spbu.matmex.ru" = true)
-printfn "%A" (is_properly_email "not_kill@mail.ru" = true)
-printfn "%A" (is_properly_email "n.o.t._.k.i.l.l@mail.ru" = true)
-printfn "%A" (is_properly_email "not-kill@mail.ru" = true)
-printfn "%A" (is_properly_email "not.kill@mail.ru" = true)
-printfn "%A" (is_properly_email "not_kill@mail.spbu.com" = true)
-// wrong examples
-printfn "%A" (is_properly_email "not@kill@mail.ru" = false)
-printfn "%A" (is_properly_email "not kill@mail.ru" = false)
-printfn "%A" (is_properly_email "not_kill@mail..ru" = false)
-printfn "%A" (is_properly_email "not_kill@mail.ruru" = false)
-printfn "%A" (is_properly_email "not_kill.@mail.ru" = false)
-printfn "%A" (is_properly_email "not_kill.mail.ru" = false)
-printfn "%A" (is_properly_email "not_kill@.mail.ru" = false)
-printfn "%A" (is_properly_email "not_kill@mail.r" = false)
+
+[<TestFixture>]
+type MailTest() = 
+    
+    let oldRightTests = [
+                            "LoZoV.PeTeR@spb.spbu.matmex.ru"
+                            "not_kill@mail.ru"
+                            "n.o.t._.k.i.l.l@mail.ru"
+                            "not-kill@mail.ru"
+                            "not.kill@mail.ru"
+                            "not_kill@mail.spbu.com"
+                        ]
+
+    let oldWrongTests = [
+                            "not@kill@mail.ru"
+                            "not kill@mail.ru"
+                            "not_kill@mail..ru" 
+                            "not_kill@mail.ruru"
+                            "not_kill.@mail.ru"
+                            "not_kill.mail.ru"
+                            "not_kill@.mail.ru"
+                            "not_kill@mail.r"
+                        ]
+
+    [<Test>]
+    member this.``Старые тесты`` 
+        ()= oldRightTests |> List.iter (fun x -> isProperlyEmail x |> should be True)
+            oldWrongTests |> List.iter (fun x -> isProperlyEmail x |> should be False)
+
+    [<Test>]
+    member this.``Очень маленький, но правильный e-mail`` 
+        ()= isProperlyEmail "a@b.cc" |> should be True
+
+    [<Test>]
+    member this.``Просто обязан быть распознанным`` 
+        ()= isProperlyEmail "victor.polozov@mail.ru" |> should be True
+
+    [<Test>]
+    member this.``Самый обычный e-mail`` 
+        ()= isProperlyEmail "my@domain.info" |> should be True
+
+    [<Test>]
+    member this.``Первый символ '_', это норма`` 
+        ()= isProperlyEmail "_.1@mail.com" |> should be True
+
+    [<Test>]
+    member this.``Большой и страшный, но правильный e-mail`` 
+        ()= isProperlyEmail "coins_department@hermitage.museum" |> should be True
+
+    [<Test>]
+    member this.``Хоть маленький, а неправильный e-mail`` 
+        ()= isProperlyEmail "a@b.c" |> should be False
+
+    [<Test>]
+    member this.``Две точки подряд в e-mail'е не проходят`` 
+        ()= isProperlyEmail "a..b@mail.ru" |> should be False
+
+    [<Test>]
+    member this.``Начинаться с точки e-mail не может`` 
+        ()= isProperlyEmail ".a@mail.ru" |> should be False
+    
+    [<Test>]
+    member this.``Не допускается длинный домен верхнего уровня`` 
+        ()= isProperlyEmail ".yo@domain.somedomain" |> should be False
+
+    [<Test>]
+    member this.``Нельзя начинать e-mail с цифр`` 
+        ()= isProperlyEmail "1@mail.ru" |> should be False   
