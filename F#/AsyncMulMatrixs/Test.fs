110ce41b6c733edf65539df58972164193e48e80
Лозов Пётр <not_kill@mail.ru>
2013-12-13 13:30:11 +0400

    Незначительные изменения

--- F#/AsyncMulMatrixs/Test.fs
+++ F#/AsyncMulMatrixs/Test.fs
@@ -11,9 +11,8 @@ type Test() =
     static let arrWorkflows = [1..8]
 
     static member private CreateRndMatrix size =
-        [0..size - 1]
-        |> List.map (fun _ -> List.map (fun _ -> rnd.Next 9) [0..size - 1])
-        |> array2D
+        (fun _ _ -> rnd.Next 9)
+        |> Array2D.init size size
         |> MyMatrix.Create
 
     static member private CreateListRndMatrix() =
@@ -22,16 +21,16 @@ type Test() =
     static member private CreateListTwoRndMatrix() =
         List.zip (Test.CreateListRndMatrix()) (Test.CreateListRndMatrix())
 
-    static member private Test a b n =
+    static member private TestForOneWorkflow a b n =
         time.Restart()         
         ignore <| MyMatrix.Mul a b n
         time.Stop()
-        let endWord = if n = 1 then "е " else "ах"
-        printfn "   При %d поток%s время работы составило: %A" n endWord time.Elapsed
+        let endOfWord = if n = 1 then "е " else "ах"
+        printfn "   При %d поток%s время работы составило: %A" n endOfWord time.Elapsed
 
     static member private TestForAllWorkflows (a : MyMatrix, b : MyMatrix) =
         printfn "\nРазмер матрицы: %d" a.Height
-        List.iter (Test.Test a b) arrWorkflows
+        List.iter (Test.TestForOneWorkflow a b) arrWorkflows
 
     static member Run() =
         List.iter Test.TestForAllWorkflows <| Test.CreateListTwoRndMatrix()
