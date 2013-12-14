16ab1eac50bff527b2d9ff8a416ab9c9a9d6eaa1
Лозов Пётр <not_kill@mail.ru>
2013-12-14 04:57:13 +0400

    Выложил Web Clawler.

---
+++ F#/WebClawler/Shell.fs
@@ -0,0 +1,44 @@
+﻿module Shell
+
+open WebCrawler
+open System.Windows.Forms
+open System.Drawing
+
+type ShellForWebCrawler() as this =
+    inherit Form(
+                    Text            = "Web Crawler",
+                    Width           = 420,
+                    Height          = 150,
+                    FormBorderStyle = FormBorderStyle.Fixed3D,
+                    MaximizeBox     = false
+                )
+
+    let infoInput = new Label(
+                            Left      = 100,
+                            Top       = 5,
+                            Width     = 200,
+                            Text      = "Введите URI:",
+                            TextAlign = ContentAlignment.MiddleCenter
+                            
+                        )
+    
+    let input = new TextBox(
+                                Left  = 50,
+                                Top   = 25,
+                                Width = 300
+                           )
+
+    let start = new Button(
+                            Left   = 100,
+                            Top    = 55,
+                            Width  = 200,
+                            Height = 40,
+                            Text   = "Запуск"
+                          )
+
+    let webCrawler = new WebCrawler()
+
+    do  this.Controls.AddRange([|input; infoInput; start|])
+        start.Click.Add(fun _ -> webCrawler.Crawle(input.Text))
+        
+do Application.Run(new ShellForWebCrawler())
---
+++ F#/WebClawler/WebCrawler.fs
@@ -0,0 +1,82 @@
+﻿module WebCrawler
+
+open Microsoft.FSharp.Control.WebExtensions
+open System
+open System.IO
+open System.Net
+open System.Collections.Concurrent
+open System.Text.RegularExpressions
+
+type WebCrawler() =
+    
+    let attendedLinks = new ConcurrentDictionary<string, unit>()
+    let downloadedPictures = new ConcurrentDictionary<string, unit>()
+                
+    static let extensions = "\.(:?ico|bmp|jpeg|jpg|png|gif)"
+    static let name       = sprintf "[a-zA-Z0-9._-]+%s" extensions
+    static let regUri     = "(:?https?\:)?[a-zA-Z0-9./_-]+"
+    static let regPic     = sprintf "(:?(:?href=\")?(?<pic>%s%s))" regUri extensions
+    static let regLink    = sprintf "(:?href=\"(?<link>%s)\")" regUri
+    static let regAll     = sprintf "%s|%s" regPic regLink
+    static let regex      = new Regex(regAll)
+    static let regexName  = new Regex(name)
+
+    member private x.GetHTML(link : string) = 
+        async {
+                try let! html = new Uri(link) |> (new WebClient()).AsyncDownloadString  in return html
+                with | _ -> return String.Empty
+              } 
+
+    member private x.GetLinks(link : string) (host : string) (html : string) =
+        async {
+                let res = regex.Matches(html)
+                let get (s : string) = 
+                    [for l in [for i in res -> i.Groups.[s]] -> l.Value]
+                    |> List.filter ((<>) String.Empty) 
+                    |> List.map (fun s -> if s.StartsWith "/" then sprintf "%s%s" host s else s)
+                let filterLinks = List.filter (fun (s : string) -> s.IndexOf link <> -1)
+                return "link" |> get |> filterLinks, "pic" |> get
+              }
+
+    member private x.DownloadPicture(link : string) =
+        let createName (link : string) = link.Replace("/", " ").Replace(":", " ")
+        async {
+                try if not <| downloadedPictures.ContainsKey(link) then
+                        let uri = new Uri(link)
+                        downloadedPictures.GetOrAdd(link, ())
+                        let name = regexName.Match(uri.OriginalString).Value
+                        (new WebClient()).DownloadFileAsync(uri, sprintf "Picture/%s" <| createName link)
+                with _ -> ()
+              }
+
+    member private x.PrivateCrawle (host : string) (link : string) =
+        async {
+                let removeAttendedLinks = List.filter (not << attendedLinks.ContainsKey)
+                attendedLinks.GetOrAdd(link, ())
+                let! html = x.GetHTML link
+                let! links, pics = x.GetLinks link host html
+
+                pics
+                |> List.map x.DownloadPicture
+                |> Async.Parallel
+                |> Async.RunSynchronously
+                |> ignore
+
+                links
+                |> removeAttendedLinks
+                |> List.map (x.PrivateCrawle host)
+                |> Async.Parallel
+                |> Async.RunSynchronously
+                |> ignore
+              }
+
+    member x.Crawle(link : string) =
+
+        attendedLinks.Clear()
+        downloadedPictures.Clear()
+        Directory.CreateDirectory("Picture") |> ignore
+        let uri = new Uri(link)  
+        let host = sprintf "%s://%s" uri.Scheme uri.Host
+
+        x.PrivateCrawle host link
+        |> Async.RunSynchronously
